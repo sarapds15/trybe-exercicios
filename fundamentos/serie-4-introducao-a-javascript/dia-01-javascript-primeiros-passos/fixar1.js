034d14e1023369a0f2ff2f046f0431a9a0411fbe
sarapds15 <sarapds15@gmail.com>
2022-09-21 16:34:39 -0300

    criação fixar1.js

---
+++ fundamentos/serie-4-introducao-a-javascript/dia-01-javascript-primeiros-passos/fixar1.js
@@ -0,0 +1,13 @@
+const myName = "Sara";
+const birthCity = "São Paulo";
+let birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Alagoas";
+console.log(birthCity);

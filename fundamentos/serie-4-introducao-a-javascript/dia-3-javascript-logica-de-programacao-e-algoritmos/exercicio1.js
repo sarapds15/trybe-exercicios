5489d52fe83a9792315e7df9763f23f842cc3d88
sarapds15 <sarapds15@gmail.com>
2022-09-23 18:42:29 -0300

    ultimo ex feito

--- fundamentos/serie-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/serie-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -17,22 +17,40 @@
 
 //3 - retornar a maior e a menor palavra:
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let maiorPalavra = array[0];
-let menorPalavra = array [0]
+// let maiorPalavra = array[0];
+// let menorPalavra = array [0]
 
-for (let index = 0; index < array.length; index += 1) {
-    if(array[index].length > maiorPalavra.length){
-        maiorPalavra = array[index];
+// for (let index = 0; index < array.length; index += 1) {
+//     if(array[index].length > maiorPalavra.length){
+//         maiorPalavra = array[index];
+//     }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if(array[index].length < menorPalavra.length){
+//         menorPalavra = array[index];
+//     }
+// }
+
+// console.log(maiorPalavra);
+// console.log(menorPalavra);
+
+// 4. escreva os numeros primos entre 2 e 50:
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
     }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
 }
 
-for (let index = 0; index < array.length; index += 1) {
-    if(array[index].length < menorPalavra.length){
-        menorPalavra = array[index];
-    }
-}
-
-console.log(maiorPalavra);
-console.log(menorPalavra);
+console.log(biggestPrimeNumber);

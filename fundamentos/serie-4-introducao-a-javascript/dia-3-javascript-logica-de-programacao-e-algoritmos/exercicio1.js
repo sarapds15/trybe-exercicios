e965ce2a63c96b80c25a5e44b4cc864d902631d1
sarapds15 <sarapds15@gmail.com>
2022-09-23 18:28:28 -0300

    exercicio 3 resolvido

--- fundamentos/serie-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/serie-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -1,4 +1,4 @@
-// fatorial de 10:
+// 1- fatorial de 10:
 
 // let fatorial = 1;
 // for (let index = 10; index > 0; index -= 1) {
@@ -6,3 +6,33 @@
 // }
 // console.log(fatorial);
 
+// 2- crie um anagrama com a palavra 'tryber':
+
+// let word = 'tryber';
+// let wordInverted = " ";
+// for (let index = 0; index < word.length; index += 1) {
+//     wordInverted += word[word.length - 1 - index];
+// }
+// console.log(wordInverted);
+
+//3 - retornar a maior e a menor palavra:
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array [0]
+
+for (let index = 0; index < array.length; index += 1) {
+    if(array[index].length > maiorPalavra.length){
+        maiorPalavra = array[index];
+    }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+    if(array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);

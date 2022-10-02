e722509b65a9efb755314e88c724e536db392557
sarapds15 <sarapds15@gmail.com>
2022-10-02 20:38:05 -0300

    resolução dos exercicios novamente

---
+++ fundamentos/serie-4-introducao-a-javascript/dia-03-javaScript-logica-De-Programacao-E-Algoritmos/script.js
@@ -0,0 +1,53 @@
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1){
+    fatorial *= index;
+}
+// console.log(fatorial);
+
+let word = 'tryber';
+let inverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+    inverseWord += word[word.length - 1 - index];
+}
+// console.log (inverseWord);
+
+inverseWord = word.split('').reverse().join('');
+
+// console.log (inverseWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (index = 0; index < array.length; index += 1) {
+    if (array[index].length > biggestWord.length){
+        biggestWord = array[index];
+    }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < smallestWord.length) {
+        smallestWord = array[index];
+    }
+}
+
+// console.log (biggestWord);
+// console.log (smallestWord);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);

1944a388609683518b6d384832d957eb30be9660
sarapds15 <sarapds15@gmail.com>
2022-10-03 16:24:26 -0300

    resolução da parte 1 do calendário

--- fundamentos/serie-5-javaScript-dom-eventos-e-web-storage/dia-03-javaScript-eventos/script.js
+++ fundamentos/serie-5-javaScript-dom-eventos-e-web-storage/dia-03-javaScript-eventos/script.js
@@ -9,9 +9,31 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  }
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  
+} 
+createDaysOfTheWeek();
+
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth() {
+    const getDayList = document.querySelector('#days');
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+        let day = decemberDaysList[index];
+        let dayItem = document.createElement('li');
+        dayItem.innerText = day;
+
+        if (day === 24 || day === 31) {
+            dayItem.className = 'day holiday';
+            getDayList.appendChild(dayItem); 
+        } else if (day === 4 || day === 11 || day === 18) {
+            dayItem.className = 'day friday';
+            getDayList.appendChild(dayItem);
+        } else if (day === 25) {
+            dayItem.className = 'day holiday friday';
+            getDayList.appendChild(dayItem);
+        } else {
+            dayItem.className = 'day';
+            getDayList.appendChild(dayItem);
+        }
+    }
+}
+createDaysOfTheMonth()
